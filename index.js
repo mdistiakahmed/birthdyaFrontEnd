486a5413f26605decd47372d5e7c3d95a51654ca
istinishat <istinishat@gmail.com>
2021-07-23 12:29:10 +0600

    Remove unnecessary code

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 const cumulativeSums = [31, 59, 90, 120, 151, 181, 212, 243, 274, 304, 335, 365];
 const days = [1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
 
+
+// Set month select options
 let selectBox = document.getElementById('months');
 for(let i = 0, l = months.length; i < l; i++){
   let month = months[i];
   selectBox.options.add( new Option(month) );
 }
 
+// Set day select options
 selectBox = document.getElementById('days');
 for(let i = 0, l = days.length; i < l; i++){
   let day = days[i];
@@ -18,7 +21,7 @@ for(let i = 0, l = days.length; i < l; i++){
 let allBirthdayEvents = new Array();
 
 function findEventsOnBirthDay(){
-    console.log('I am here');
+    // Calculate day (1~365) from input month and day
     let m = document.getElementById("months").value;
     let d = parseInt(document.getElementById("days").value);
     let cum_day = 0;
@@ -33,39 +36,25 @@ function findEventsOnBirthDay(){
         }
     }
     cum_day = cum_day + d;
-    fetch("http://localhost:8080/api/test")
-        .then(response => {
-           // console.log(response);
-            return response.text();
-        }).then(json=>{
-            //alert(json);
-        })
-    //alert("Direct Access: "+"months : "+ m + ", days : " + d + ", cum day : "+ cum_day);
+    // Call the GET Api with day as parameter
     fetch("http://localhost:8080/api/photos/getAllPhoto?day="+cum_day)
         .then(response => {
-           // console.log(response);
            return response.json();
         }).then(json=>{
-            console.log("printing..");
+            // Clear the screen
             removeelement();
+            // Set title, descriptino and photo as a list 
             Object.entries(json).forEach(([key, value]) => {
-            let title = value["title"];
-            let description = value["description"];
-            let myPhoto = value["image"].data;
-            console.log(title);
-            console.log(description);
-            console.log(myPhoto);
-            console.log('-----------');
-            let x = "data:image/;base64,"+myPhoto;
-            let imgString = "<img  src=\"" + x +  "\">";
-            var completelist= document.getElementById("theEventList");
-            completelist.innerHTML += imgString;
-            completelist.innerHTML += "<li>" + title + "</li>";
-            completelist.innerHTML += "<li>" + description + "</li>";
-            
-           // document.getElementById("one").setAttribute("src", x); 
+                let title = value["title"];
+                let description = value["description"];
+                let myPhoto = value["image"].data;
 
-            //addelement();
+                let x = "data:image/;base64,"+myPhoto;
+                let imgString = "<img  src=\"" + x +  "\">";
+                var completelist= document.getElementById("theEventList");
+                completelist.innerHTML += imgString;
+                completelist.innerHTML += "<li>" + title + "</li>";
+                completelist.innerHTML += "<li>" + description + "</li>";
             })
            
         }).catch(function(error){
@@ -73,13 +62,7 @@ function findEventsOnBirthDay(){
         })
 }
 
-
-function addelement() {
-    var completelist= document.getElementById("theEventList");
-    
-    completelist.innerHTML += "<li>Item " + 1 + "</li>";
-}
-
+// clear the display list
 function removeelement() {
     var completelist= document.getElementById("theEventList");
     
--- upload.js
+++ upload.js
@@ -1,18 +1,22 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August",
     "September", "October", "November", "December"];
 const days = [1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28];
+
+// Set month select options
 let selectBox = document.getElementById('months');
 for(let i = 0, l = months.length; i < l; i++){
     let month = months[i];
     selectBox.options.add( new Option(month) );
 }
 
+// Set day select options
 selectBox = document.getElementById('days');
     for(let i = 0, l = days.length; i < l; i++){
     let day = days[i];
     selectBox.options.add( new Option(day) );
 }
 
+// Submit the form
 formElem.onsubmit = async (e) => {
     e.preventDefault(); 
     let response = await fetch('http://localhost:8080/api/photos/addPhoto1', {
